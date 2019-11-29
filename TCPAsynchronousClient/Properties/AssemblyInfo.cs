fe1d24dffc9548bc470145341c2c9d2500223a25
PasanBhanu <pasanbguruge@hotmail.com>
2019-11-29 10:12:55 +0530

    Release

--- TCPAsynchronousClient/Properties/AssemblyInfo.cs
+++ TCPAsynchronousClient/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TCP Asynchronous Client")]
+[assembly: AssemblyTitle("TCP Asynchronous Client - Debug Mode")]
 [assembly: AssemblyDescription("TCP Asynchronous Client for C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Softink Lab")]
