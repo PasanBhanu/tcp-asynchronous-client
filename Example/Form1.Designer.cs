141090c827c93fbbc25889f3b661a98d5dada7c0
PasanBhanu <pasanbguruge@hotmail.com>
2019-12-02 12:11:29 +0530

    Update Compatibility

---
+++ Example/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.rtfLog = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.txtWrite = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblConnected = new System.Windows.Forms.Label();
+            this.lblRead = new System.Windows.Forms.Label();
+            this.lblWrite = new System.Windows.Forms.Label();
+            this.timClear = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(133, 81);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(91, 29);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(198, 20);
+            this.txtIpAddress.TabIndex = 3;
+            this.txtIpAddress.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(91, 55);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(198, 20);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "5555";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.txtIpAddress);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 128);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TCP Port Details";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(214, 81);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // rtfLog
+            // 
+            this.rtfLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtfLog.Location = new System.Drawing.Point(331, 12);
+            this.rtfLog.Name = "rtfLog";
+            this.rtfLog.Size = new System.Drawing.Size(501, 417);
+            this.rtfLog.TabIndex = 6;
+            this.rtfLog.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnWrite);
+            this.groupBox2.Controls.Add(this.txtWrite);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 146);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(306, 99);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Write Data";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Enabled = false;
+            this.btnWrite.Location = new System.Drawing.Point(214, 55);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 2;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // txtWrite
+            // 
+            this.txtWrite.Location = new System.Drawing.Point(91, 29);
+            this.txtWrite.Name = "txtWrite";
+            this.txtWrite.Size = new System.Drawing.Size(198, 20);
+            this.txtWrite.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Data";
+            // 
+            // lblConnected
+            // 
+            this.lblConnected.BackColor = System.Drawing.Color.Red;
+            this.lblConnected.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnected.ForeColor = System.Drawing.Color.White;
+            this.lblConnected.Location = new System.Drawing.Point(12, 406);
+            this.lblConnected.Name = "lblConnected";
+            this.lblConnected.Size = new System.Drawing.Size(100, 23);
+            this.lblConnected.TabIndex = 8;
+            this.lblConnected.Text = "Disconnected";
+            this.lblConnected.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRead
+            // 
+            this.lblRead.BackColor = System.Drawing.Color.Red;
+            this.lblRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRead.ForeColor = System.Drawing.Color.White;
+            this.lblRead.Location = new System.Drawing.Point(118, 406);
+            this.lblRead.Name = "lblRead";
+            this.lblRead.Size = new System.Drawing.Size(94, 23);
+            this.lblRead.TabIndex = 9;
+            this.lblRead.Text = "Read";
+            this.lblRead.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblRead.Visible = false;
+            // 
+            // lblWrite
+            // 
+            this.lblWrite.BackColor = System.Drawing.Color.Red;
+            this.lblWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWrite.ForeColor = System.Drawing.Color.White;
+            this.lblWrite.Location = new System.Drawing.Point(218, 406);
+            this.lblWrite.Name = "lblWrite";
+            this.lblWrite.Size = new System.Drawing.Size(83, 23);
+            this.lblWrite.TabIndex = 10;
+            this.lblWrite.Text = "Write";
+            this.lblWrite.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblWrite.Visible = false;
+            // 
+            // timClear
+            // 
+            this.timClear.Enabled = true;
+            this.timClear.Interval = 1000;
+            this.timClear.Tick += new System.EventHandler(this.timClear_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 441);
+            this.Controls.Add(this.lblWrite);
+            this.Controls.Add(this.lblRead);
+            this.Controls.Add(this.lblConnected);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.rtfLog);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Emulator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.RichTextBox rtfLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox txtWrite;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblConnected;
+        private System.Windows.Forms.Label lblRead;
+        private System.Windows.Forms.Label lblWrite;
+        private System.Windows.Forms.Timer timClear;
+    }
+}
+
---
+++ Example/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+using TCP_Asynchronous_Client;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        // For Display Data in Text Box and Info - UI Thread Invoke
+        public delegate void AddLogDeligate(string data);
+        public AddLogDeligate AddLog;
+        public delegate void AddNotificationDelegate(int type, bool status);
+        public AddNotificationDelegate UpdateStatusIcons;
+
+        // Client Object
+        AsynchronousClient tcp;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Deligate Methods for UI Thread Invoke
+            AddLog = new AddLogDeligate(Log);
+            UpdateStatusIcons = new AddNotificationDelegate(StatusUpdate);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tcp = new AsynchronousClient(txtIpAddress.Text, int.Parse(txtPort.Text));
+                tcp.OnConnectEvent += new AsynchronousClient.OnConnectEventHandler(OnConnect);
+                tcp.OnDataRecievedEvent += new AsynchronousClient.DataReceivedEventHandler(OnRecieved);
+                tcp.Connect();
+            }catch (Exception ex)
+            {
+                // Catch errors in Connection and Recieve Callbacks
+                Log("Error : " + ex.ToString());
+            }
+            
+        }
+
+        // Connection Status Listner
+        private void OnConnect(bool status)
+        {
+            rtfLog.Invoke(AddLog, "Connection : " + status.ToString());
+            lblConnected.Invoke(UpdateStatusIcons, 1, status);
+        }
+
+        // Data Recieved Listner
+        private void OnRecieved(string data)
+        {
+            lblRead.Invoke(UpdateStatusIcons, 3, true);
+            rtfLog.Invoke(AddLog, "Recieved : " + data);
+        }
+
+        // UI Thread Invoker
+        private void StatusUpdate(int type, bool status)
+        {
+            switch (type)
+            {
+                case 1:
+                    if (status)
+                    {
+                        lblConnected.Text = "Connected";
+                        lblConnected.BackColor = System.Drawing.Color.Green;
+                        btnConnect.Enabled = false;
+                        btnDisconnect.Enabled = true;
+                        btnWrite.Enabled = true;
+                    }
+                    else
+                    {
+                        lblConnected.Text = "Disconnected";
+                        lblConnected.BackColor = System.Drawing.Color.Red;
+                        btnConnect.Enabled = true;
+                        btnDisconnect.Enabled = false;
+                        btnWrite.Enabled = false;
+                    }
+                    break;
+
+                case 2:
+                    if (status)
+                    {
+                        lblWrite.Visible = true;
+                    }
+                    else
+                    {
+                        lblWrite.Visible = false;
+                    }
+                    break;
+
+                case 3:
+                    if (status)
+                    {
+                        lblRead.Visible = true;
+                    }
+                    else
+                    {
+                        lblRead.Visible = false;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // UI Thread Invoker
+        private void Log(string _data)
+        {
+            rtfLog.Text = rtfLog.Text + _data + "\n\n";
+            rtfLog.SelectionStart = rtfLog.Text.Length;
+            rtfLog.ScrollToCaret();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            tcp.Disconnect();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            // UI Notification
+            StatusUpdate(2, true);
+
+            // Write to TCP
+            try
+            {
+                if (tcp.Write(txtWrite.Text))
+                {
+                    Log("Write (Success) : " + txtWrite.Text);
+                }
+                else
+                {
+                    Log("Write (Failed) : Disconnected");
+                }
+            }catch(Exception ex)
+            {
+                // Catch errors in Sending Data
+                Log("Error : " + ex.ToString());
+            }
+            
+        }
+
+        // Clear Notifications
+        private void timClear_Tick(object sender, EventArgs e)
+        {
+            lblWrite.Visible = false;
+            lblRead.Visible = false;
+        }
+    }
+}
